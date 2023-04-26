6de69772f7c674fc0e6a305752ca2d4b5586b324
THIAGO VIUDES CARSADO <129440601+viudes-13@users.noreply.github.com>
2023-04-26 15:29:02 -0300

    recursos Jquery

--- js/contato.js
+++ js/contato.js
@@ -2,11 +2,16 @@
 const formulario = document.querySelector("form");
 const campoCep = formulario.querySelector("#cep");
 const campoEndereco = formulario.querySelector("#endereco");
+const campoTelefone = formulario.querySelector("#telefone");
 const campoBairro = formulario.querySelector("#bairro");
 const campoCidade = formulario.querySelector("#cidade");
 const campoEstado = formulario.querySelector("#estado");
-const status = formulario.querySelector("#status");
+const mensagem = formulario.querySelector("#status");
 const botaoLocalizar = formulario.querySelector("#localizar-cep");
+
+/*ativação das macaras co jQuer Mask*/
+$(campoCep).mask("00000-000")
+$(campoTelefone).mask("(00) 0000-0000")
 
 /* Monitorando o evento de acionamento do botão localizar cep */
 botaoLocalizar.addEventListener("click", function(event){
@@ -40,10 +45,14 @@ botaoLocalizar.addEventListener("click", function(event){
         // Se existir o indicador "erro" no objeto dados
         if( "erro" in dados ){
             // Apresentamos a mensagem abaixo
-            console.log("CEP não encontrado!");
+            mensagem.innerHTML = "CEP não encontrado!";
+            mensagem.style.color = "red";
+            campoCep.focus();
         } else {
             // Senão, o cep existe então mostramos:
-            console.log("CEP encontrado!");
+            mensagem.innerHTML = "CEP encontrado!";
+            mensagem.style.color = "blue";
+
 
             campoEndereco.value = dados.logradouro;
             campoBairro.value = dados.bairro;
